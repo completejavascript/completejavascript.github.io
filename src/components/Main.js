6fc20d297f25b93576b04da95ccc21fa56fc320e
completejavascript <completejavascript.super@gmail.com>
2019-06-28 08:01:27 +0700

    Change Header to Intro

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './Header';
+import Intro from './Intro';
 import About from './About';
 import './Main.css';
 
 const Main = () => {
   return (
     <div className="app-main float-right transition">
-      <Header />
+      <Intro />
       <About />
     </div>
   )
