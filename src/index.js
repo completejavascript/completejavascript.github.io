dce7bc022f9f2a1be0157021f2c9b398f10b8a01
completejavascript <completejavascript.super@gmail.com>
2019-06-30 23:44:55 +0700

    Remove scroll smooth from index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './lib/css/shiba.min.css';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
