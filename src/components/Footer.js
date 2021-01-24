cb3be2a4823f04e7fb92ae39cd20a5eb1c045c6e
lampv4 <v.lampv4@vinsmart.net.vn>
2021-01-24 11:59:30 +0700

    Update footer copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ExternalLink from './ExternalLink';
+import React from "react";
+import ExternalLink from "./ExternalLink";
 
 const Footer = () => {
   return (
     <footer className="container-full background-color-dark-grey color-white text-center padding-top-half padding-bottom-half">
-        <small>Copyright &copy; 2018 - 2020 by
-          <ExternalLink href="https://completejavascript.com/gioi-thieu/"> Lam Pham</ExternalLink>. 
-          All Rights Reserved.
-        </small>
+      <small>
+        Copyright &copy; 2018 - 2021 by
+        <ExternalLink href="https://completejavascript.com/gioi-thieu/">
+          {" "}
+          Lam Pham
+        </ExternalLink>
+        . All Rights Reserved.
+      </small>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
