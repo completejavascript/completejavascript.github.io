994aed0bb51d3f426fa866709ab74887380f165b
lampv4 <v.lampv4@vinsmart.net.vn>
2021-01-24 11:43:33 +0700

    Update about and layout about, contact responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { IconDownload } from './Icons';
-import SectionTitle from './SectionTitle';
-import SectionDescription from './SectionDescription';
-import ExternalLink from './ExternalLink';
+import React from "react";
+import { IconDownload } from "./Icons";
+import SectionTitle from "./SectionTitle";
+import SectionDescription from "./SectionDescription";
+import ExternalLink from "./ExternalLink";
+import "./About.css";
 
 const SkillBadge = ({ subject, percentage }) => (
-  <div className={`border border-default inline-block margin-right-half 
-    margin-bottom-half padding-left-half padding-right-half`}>
-    {subject}{" "}{percentage}
+  <div
+    className={`border border-default inline-block margin-right-half 
+    margin-bottom-half padding-left-half padding-right-half`}
+  >
+    {subject} {percentage}
   </div>
-)
+);
 
 const Skills = () => (
   <div className="margin-bottom">
@@ -29,7 +32,7 @@ const Skills = () => (
     <SkillBadge subject="MongoDB &amp; Mongoose" percentage={80} />
     <SkillBadge subject="QT/QML" percentage={85} />
   </div>
-)
+);
 
 const About = () => {
   return (
@@ -37,44 +40,82 @@ const About = () => {
       <div className="col-xs-12 margin-top-3">
         <SectionTitle title="About" />
         <SectionDescription content="A little bit about me." />
-        <div className="padding-left-3 padding-right-3">
+        <div className="about-content">
           <p>
-            Hi, My name is <strong className="color-red">Lam Pham</strong> from Vietnam. I am a Software Engineer with a focus on <strong className="color-red">C/C++</strong>.
-              Beside, I am really passionate about <strong className="color-blue">JavaScript</strong> and
-              <strong className="color-blue"> Web Development</strong>. I got a bachelor of Electronics and Telecommunication Engineering at
-            Ha Noi Uninversity of Science and Technology (2011 - 2016). And 2016 - present, I work for Samsung Vietnam Mobile R&amp;D Center (SVMC)
-            as a C/C++ Software Engineer.
-            </p>
+            Hi, My name is <strong className="color-red">Lam Pham</strong> from
+            Vietnam. I am a Software Engineer with a focus on{" "}
+            <strong className="color-red">C/C++</strong>. Beside, I am really
+            passionate about <strong className="color-blue">JavaScript</strong>{" "}
+            and
+            <strong className="color-blue"> Web Development</strong>. I got a
+            bachelor of Electronics and Telecommunication Engineering at Ha Noi
+            Uninversity of Science and Technology (2011 - 2016).
+          </p>
+          <p>
+            From 06/2016 - 03/2019, I work for Samsung Vietnam Mobile R&amp;D
+            Center (SVMC) as a C/C++ Software Engineer. And 03/2019 - present, I
+            work for VinSmart as a C/C++, Qt/QML Software Engineer and Front-end
+            web developer with ReactJS.
+          </p>
           <p>
             As you can see, this website is my personal portfolio webpage, which
-              I created while learning Web Development from <ExternalLink href="https://www.freecodecamp.org">freeCodeCamp</ExternalLink> in my spare time.
-            And from now on, I am going to share my latest work consistently. Each project consists of its link for preview on Github
-              and <strong className="color-red">how-to</strong>, which I explain how I created it.
-            In fact, before digging deeper into freeCodeCamp or Web Development, I self-learned JavaScript, HTML5 &amp; CSS3, Bootstrap &amp; jQuery,
-            AngularJS and NodeJS &amp; ExpressJS... So, below are some informations about my that I self-evaluate honestly:
-            </p>
+            I created while learning Web Development from{" "}
+            <ExternalLink href="https://www.freecodecamp.org">
+              freeCodeCamp
+            </ExternalLink>{" "}
+            in my spare time. And from now on, I am going to share my latest
+            work consistently. Each project consists of its link for preview on
+            Github and <strong className="color-red">how-to</strong>, which I
+            explain how I created it. In fact, before digging deeper into
+            freeCodeCamp or Web Development, I self-learned JavaScript, HTML5
+            &amp; CSS3, Bootstrap &amp; jQuery, AngularJS and NodeJS &amp;
+            ExpressJS... So, below are some informations about my that I
+            self-evaluate honestly:
+          </p>
 
           <Skills />
 
-          <p>Besides, I love coding and blogging. I try to code everyday and write blogs to share my experiences to people.
-            Here are my blogs (in Vietnamese):
-              <span><ExternalLink href="https://completejavascript.com"> Complete JavaScript</ExternalLink></span> <strong>·</strong>
-            <span><ExternalLink href="http://thuattoan.phamvanlam.com"> Algorithm</ExternalLink></span> <strong>·</strong>
-            <span><ExternalLink href="http://phamvanlam.com"> Personal Blog</ExternalLink></span>.
-            </p>
           <p>
-            I enjoy instrument musical. In my free time, I usually play Ukulele. And, I like Street Workout. I am training it four times a week to get fit and healthy.
-            Thanks for visiting my website!
-            </p>
+            Besides, I love coding and blogging. I try to code everyday and
+            write blogs to share my experiences to people. Here are my blogs (in
+            Vietnamese):
+            <span>
+              <ExternalLink href="https://completejavascript.com">
+                {" "}
+                Complete JavaScript
+              </ExternalLink>
+            </span>{" "}
+            <strong>·</strong>
+            <span>
+              <ExternalLink href="http://thuattoan.phamvanlam.com">
+                {" "}
+                Algorithm
+              </ExternalLink>
+            </span>{" "}
+            <strong>·</strong>
+            <span>
+              <ExternalLink href="http://phamvanlam.com">
+                {" "}
+                Personal Blog
+              </ExternalLink>
+            </span>
+            .
+          </p>
+          <p>
+            I enjoy instrument musical. In my free time, I usually play Ukulele.
+            And, I like Street Workout. I am training it four times a week to
+            get fit and healthy. Thanks for visiting my website!
+          </p>
           <p className="text-center margin-bottom-3">
             <ExternalLink href="/cv/LamPham.pdf" className="btn btn-primary">
-              <IconDownload color="#FFF" /><strong className="padding-left-half">MY CV</strong>
+              <IconDownload color="#FFF" />
+              <strong className="padding-left-half">MY CV</strong>
             </ExternalLink>
           </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import SectionTitle from './SectionTitle';
-import SectionDescription from './SectionDescription';
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import SectionDescription from "./SectionDescription";
+import "./Contact.css";
 
 const Contact = () => {
   return (
@@ -8,22 +9,48 @@ const Contact = () => {
       <div className="container-small row">
         <div className="col-xs-12 margin-top-3">
           <SectionTitle title="Contact" />
-          <SectionDescription 
+          <SectionDescription
             content="If you would like to get in touch with me, please visit my social networks or send me a message. 
             Thanks a lot!"
           />
-          <div className="margin-left-3 margin-right-3 margin-bottom-3 padding-bottom">
-            <form method="POST" action="https://formspree.io/completejavascript.super@gmail.com">
-              <input className="background-color-white" type="text" id="yname" name="Name" placeholder="Name" required />
-              <input className="background-color-white"  type="email" id="yemail" name="Email" placeholder="Email" required />
-              <textarea className="background-color-white"  type="text" id="ymsg" name="Message" placeholder="Message" required></textarea>
-              <button className="btn-full btn-primary" type="submit" id="send"><strong>Send Message</strong></button>
+          <div className="contact-container margin-bottom-3 padding-bottom">
+            <form
+              method="POST"
+              action="https://formspree.io/completejavascript.super@gmail.com"
+            >
+              <input
+                className="background-color-white"
+                type="text"
+                id="yname"
+                name="Name"
+                placeholder="Name"
+                required
+              />
+              <input
+                className="background-color-white"
+                type="email"
+                id="yemail"
+                name="Email"
+                placeholder="Email"
+                required
+              />
+              <textarea
+                className="background-color-white"
+                type="text"
+                id="ymsg"
+                name="Message"
+                placeholder="Message"
+                required
+              ></textarea>
+              <button className="btn-full btn-primary" type="submit" id="send">
+                <strong>Send Message</strong>
+              </button>
             </form>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Contact;
