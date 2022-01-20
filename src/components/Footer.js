275af45e7d1f394168e1c4fb55188cb44005d0d0
Lam Pham <completejavascript.super@gmail.com>
2022-01-20 20:58:20 +0700

    fix: update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from "react";
 import ExternalLink from "./ExternalLink";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="container-full background-color-dark-grey color-white text-center padding-top-half padding-bottom-half">
       <small>
-        Copyright &copy; 2018 - 2021 by
+        Copyright &copy; 2018 - {currentYear} by
         <ExternalLink href="https://completejavascript.com/gioi-thieu/">
           {" "}
           Lam Pham
